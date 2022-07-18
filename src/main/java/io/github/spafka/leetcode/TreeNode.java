6ee7ace7ea19de8f302225683c50e1b062d699d7
spafka <spafka@163.com>
2022-07-18 17:14:11 +0800

    daly 20220718

--- src/main/java/io/github/spafka/leetcode/TreeNode.java
+++ src/main/java/io/github/spafka/leetcode/TreeNode.java
@@ -1,6 +1,7 @@
 package io.github.spafka.leetcode;
 
 
+import java.util.Stack;
 import java.util.stream.IntStream;
 
 public class TreeNode<N extends TreeNode> {
@@ -10,6 +11,7 @@ public class TreeNode<N extends TreeNode> {
 
     public TreeNode() {
     }
+
 
     public TreeNode(int val, N left, N right) {
         this.val = val;
@@ -34,5 +36,59 @@ public class TreeNode<N extends TreeNode> {
 
     }
 
+    public static void inOrderIter(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {  // 如果节点为空或栈为空  则遍历结束
+            while (root != null) {  //如果根节点不为空
+                stack.push(root);  // 将根节点压栈
+                root = root.left;  //访问左子树
+            }
+            root = stack.pop(); // 存在节点为空  此时栈中从栈顶的元素一定为子树的最左边一个子节点
+            System.out.println(root.val); // 将子节点存入列表中
+            root = root.right;  //访问右节点
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = sortedArrayToBST(new int[]{1, 2, 3, 4, 5, 6});
+
+        view(root);
+        inOrderIter(root);
+    }
+
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+
+        return build(nums,0,nums.length-1);
+    }
+
+
+
+    public  static TreeNode build(int[] nums,int left,int right){
+        if (left>right){
+            return null;
+        }
+        int mid=(left+right)>>1;
+        TreeNode root = new TreeNode();
+        root.val=nums[mid];
+        root.left=build(nums,left,mid-1);
+        root.right=build(nums,mid+1,right);
+        return root;
+    }
+
+
+    public static int  depth(TreeNode root){
+
+        if (root==null){
+            return 0;
+        } else {
+            return Math.min(depth(root.left),depth(root.right))+1;
+        }
+    }
+
+    public static void view(TreeNode root){
+         BTreePrinter.printNode(root);
+    }
 
 }
